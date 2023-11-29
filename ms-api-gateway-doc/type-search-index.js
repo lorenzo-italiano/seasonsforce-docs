928cf4e273ac497b92f54126d3a506fcaaf1d5ed
romainfrezier <romainfrezier@gmail.com>
2023-11-29 15:24:48 +0100

    api gateway docs

---
+++ ms-api-gateway-doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fr.polytech","l":"App"},{"p":"fr.polytech","l":"CORSConfig"},{"p":"fr.polytech","l":"GatewayConfig"}];updateSearchResults();
